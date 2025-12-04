0267df9163d4cf0f6fab52040fa6fe7a8ac98c2b
Yoshi <pt13762104@gmail.com>
2025-12-04 10:34:30 +0700

    test test-backend-ops

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -8349,7 +8349,7 @@ int main(int argc, char ** argv) {
             continue;
         }
 
-        if (backend_filter == NULL && ggml_backend_dev_type(dev) == GGML_BACKEND_DEVICE_TYPE_CPU && mode != MODE_TEST) {
+        if (backend_filter == NULL && ggml_backend_dev_type(dev) == GGML_BACKEND_DEVICE_TYPE_CPU && mode == MODE_TEST) {
             output_printer->print_backend_init(backend_init_info(
                 i, ggml_backend_dev_count(), ggml_backend_dev_name(dev), true, "Skipping CPU backend"));
             n_ok++;
